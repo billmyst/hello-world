42e9922bebd9e6e3ffb2e813d2e59e9be71e47c3
wang <bill@WangJiumings-MacBook-Pro.local>
2017-05-10 14:48:17 +0800

    init hello world

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by WangJiuming on 2017/5/10.
+//  Copyright © 2017年 suniey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
