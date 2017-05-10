8e7e965fc41fca5f0549267dea9f1a750cbb4786
wang <bill@WangJiumings-MacBook-Pro.local>
2017-05-10 17:11:40 +0800

    init TestTool001

---
+++ TestTool001/TestTool001/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestTool001
+//
+//  Created by bill on 2017/5/10.
+//  Copyright © 2017年 suniey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
